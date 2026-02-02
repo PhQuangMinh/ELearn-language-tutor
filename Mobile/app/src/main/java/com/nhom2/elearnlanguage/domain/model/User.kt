a395338951af185e62f06182f938a01a0c39beb9
DoangVan411 <haov24722@gmail.com>
2026-02-02 12:57:45 +0700

    init Android project

---
+++ Mobile/app/src/main/java/com/nhom2/elearnlanguage/domain/model/User.kt
@@ -0,0 +1,4 @@
+package com.nhom2.elearnlanguage.domain.model
+
+class User {
+}
---
+++ Mobile/app/src/main/java/com/nhom2/elearnlanguage/presentation/ui/auth/login/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.nhom2.elearnlanguage.presentation.ui.auth.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nhom2.elearnlanguage.R
+import com.nhom2.elearnlanguage.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
